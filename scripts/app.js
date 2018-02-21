de03496d2a80fd41b749ab6973022bddc6085f5c
Jehan <jehan.tremback@gmail.com>
2018-02-21 15:05:03 -0800

    fix spacing in js

--- scripts/app.js
+++ scripts/app.js
@@ -1,100 +1,109 @@
 // @ts-check
 
-var Module = (function () {
-	var config = {
-		"ADD STUFF HERE"
-	};
-	 
-	firebase.initializeApp(config);
+var Module = (function() {
+  var config = {
+    // "ADD STUFF HERE"
+  };
 
-	var map = null;
-	var markerCluster = null; 
-	var markerArr = [];
-	var geocoder = null;
-	var address = null;
+  firebase.initializeApp(config);
 
-	var emailAddr = document.getElementById("user_email_input");
-	var firstName = document.getElementById("user_fname_input");
-	var lastName = document.getElementById("user_lname_input");
-	var country = document.getElementById("user_country_menu");
-	var city = document.getElementById("user_city_input");
-	var zipCode = document.getElementById("user_zip_code_input");
+  var map = null;
+  var markerCluster = null;
+  var markerArr = [];
+  var geocoder = null;
+  var address = null;
 
-	function initMap() {
-		// Initialize blank map with markers
-		resetView();
+  var emailAddr = document.getElementById("user_email_input");
+  var firstName = document.getElementById("user_fname_input");
+  var lastName = document.getElementById("user_lname_input");
+  var country = document.getElementById("user_country_menu");
+  var city = document.getElementById("user_city_input");
+  var zipCode = document.getElementById("user_zip_code_input");
 
-		geocoder = new google.maps.Geocoder();
+  function initMap() {
+    // Initialize blank map with markers
+    resetView();
 
-		document.getElementById('submit').addEventListener('click', function () {
-			address = city.value + " " + zipCode.value + " " + country.value;
-			geocodeAddress(geocoder, map);
-		});
-	};
+    geocoder = new google.maps.Geocoder();
 
-	function resetView() {
-		map = new google.maps.Map(document.getElementById('map'), {
-			center: {
-				lat: 15,
-				lng: 0
-			},
-			zoom: 2,
-		});
-		readFromFirebase();
+    document.getElementById("submit").addEventListener("click", function() {
+      address = city.value + " " + zipCode.value + " " + country.value;
+      geocodeAddress(geocoder, map);
+    });
+  }
 
-		markerCluster = new MarkerClusterer(map, markerArr, {imagePath: "/images/m"});
-	};
+  function resetView() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: {
+        lat: 15,
+        lng: 0
+      },
+      zoom: 2
+    });
+    readFromFirebase();
 
-	function readFromFirebase() {
-		markerArr = [];
+    markerCluster = new MarkerClusterer(map, markerArr, {
+      imagePath: "/images/m"
+    });
+  }
 
-		// Query data base for stored location
-		var fireDataBase = firebase.database().ref().child("Markers/");
+  function readFromFirebase() {
+    markerArr = [];
 
-		fireDataBase.on("child_added", function (snapshot) {
-			var storedData = snapshot.val();
-			var keys = Object.keys(storedData);
+    // Query data base for stored location
+    var fireDataBase = firebase
+      .database()
+      .ref()
+      .child("Markers/");
 
-			for (var i = 0; i < keys.length; i++) {
-				var k = keys[i];
-				var storedLat = storedData[k].Latitude;
-				var storedLng = storedData[k].Longitude;
+    fireDataBase.on("child_added", function(snapshot) {
+      var storedData = snapshot.val();
+      var keys = Object.keys(storedData);
 
-				// Updates map with stored marker
-				var marker = new google.maps.Marker({
-					position: {
-						lat: parseFloat(storedLat),
-						lng: parseFloat(storedLng)
-					}
-				});
-				marker.setMap(map);
-				markerArr.push(marker);
-			}
-			markerCluster.addMarkers(markerArr);
-		});
-	};
+      for (var i = 0; i < keys.length; i++) {
+        var k = keys[i];
+        var storedLat = storedData[k].Latitude;
+        var storedLng = storedData[k].Longitude;
 
-	// Convert address to Lat/ Lng
-	function geocodeAddress(geocoder, resultsMap) {
-		geocoder.geocode({
-			'address': address
-		}, function (results, status) {
-			if (status === 'OK') {
-				resultsMap.setCenter(results[0].geometry.location);
-				resultsMap.setZoom(14);
-				var marker = new google.maps.Marker({
-					position: results[0].geometry.location,
-					map: resultsMap
-				});
-			} else {
-				alert('Geocode was not successful for the following reason: ' + status);
-			}
-		});
-	};
+        // Updates map with stored marker
+        var marker = new google.maps.Marker({
+          position: {
+            lat: parseFloat(storedLat),
+            lng: parseFloat(storedLng)
+          }
+        });
+        marker.setMap(map);
+        markerArr.push(marker);
+      }
+      markerCluster.addMarkers(markerArr);
+    });
+  }
 
-	return {
-		initMap: initMap,
-		resetView: resetView
-	};
+  // Convert address to Lat/ Lng
+  function geocodeAddress(geocoder, resultsMap) {
+    geocoder.geocode(
+      {
+        address: address
+      },
+      function(results, status) {
+        if (status === "OK") {
+          resultsMap.setCenter(results[0].geometry.location);
+          resultsMap.setZoom(14);
+          var marker = new google.maps.Marker({
+            position: results[0].geometry.location,
+            map: resultsMap
+          });
+        } else {
+          alert(
+            "Geocode was not successful for the following reason: " + status
+          );
+        }
+      }
+    );
+  }
 
+  return {
+    initMap: initMap,
+    resetView: resetView
+  };
 })();
